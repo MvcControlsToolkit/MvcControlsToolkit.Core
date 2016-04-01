4ce83a173365365375730dd55a723c71091417d7
francesco <frankabbruzzese@gmail.com>
2016-04-01 19:48:36 +0200

    Added Providers Handling

---
+++ src/MvcControlsToolkit.Core.Options/IOptionsProvider.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Hosting;
+using Microsoft.AspNet.Http;
+
+
+
+namespace MvcControlsToolkit.Core.Options
+{
+    public interface IOptionsProvider
+    {
+        string Prefix { get; set; }
+        bool CanSave { get; set; }
+        void Save(HttpContext ctx, IOptionsDictionary dict);
+
+        void Load(HttpContext ctx, IOptionsDictionary dict);
+        
+    }
+}
---
+++ src/MvcControlsToolkit.Core.Options/ProvidersDictionary.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Http;
+
+namespace MvcControlsToolkit.Core.Options
+{
+    internal class ProvidersDictionary
+    {
+        private static IDictionary<string, List<IOptionsProvider>> allProviders = new Dictionary<string, List<IOptionsProvider>>();
+        private HashSet<IOptionsProvider> requestProviders = new HashSet<IOptionsProvider>();
+        public static void Add(IOptionsProvider provider)
+        {
+            string prefix = provider.Prefix;
+            List<IOptionsProvider> entry = null;
+            if (allProviders.TryGetValue(prefix, out entry)){
+                entry.Add(provider);
+            }
+            else {
+                var l = allProviders[prefix] = new List<IOptionsProvider>();
+                l.Add(provider);
+            }
+        }
+        public void AddToRequest(string prefix, HttpContext context, IOptionsDictionary dict)
+        {
+            foreach(var x in allProviders)
+            {
+                if (x.Key.StartsWith(prefix)){
+                    foreach (var y in x.Value)
+                    {
+                        if (!requestProviders.Contains(y))
+                        {
+                            y.Load(context, dict);
+                        }
+                    }
+                }
+                
+            }
+        }
+    }
+}
